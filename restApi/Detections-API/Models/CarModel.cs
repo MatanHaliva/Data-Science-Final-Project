223966a54178be515d051b0abc98c4e9c0475929
chenbhm100 <chenbhm100@gmail.com>
2021-05-29 20:33:11 +0300

    refactor detections api and init feedback api

---
+++ restApi/Detections-API/Models/CarModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Detections_API.Models
+{
+    [BsonIgnoreExtraElements]
+
+    public class CarModel:DetectionsBase
+    {
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
+        public string LicensePlate { get; set; }
+
+    }
+}
